0ba90e49fe7de74bb366b00bbb601df7368aacd3
johnvogen <john.vogen@verizon.net>
2016-05-09 19:21:29 -0400

    Add spell checking

--- ahroadmap_admin/ckeditor/config.js
+++ ahroadmap_admin/ckeditor/config.js
@@ -3,4 +3,6 @@ CKEDITOR.editorConfig = function( config ) {
 	config.uiColor = '#F7B42C';
 	config.height = 300;
 	config.toolbarCanCollapse = true;
+	config.disableNativeSpellChecker = false;
+
 };
